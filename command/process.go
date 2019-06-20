b2944fcd6deae18d3f9c5030c203b4b924a86b87
Levi Junkert <levi.junkert@smithrx.com>
2019-06-20 14:15:03 -0700

    Clean up file name

--- command/process.go
+++ command/process.go
@@ -125,7 +125,7 @@ func cliCommandProcess(cmd *cobra.Command, args []string) {
 }
 
 // process is the entry point for processing a dump file according to the map file.
-func process(dumpFile, mapFile, processedDumpFile string, generateSeed bool, s3file *gonymizer.S3File) (err error) {
+func process(dumpFile, mapFile, processedFile string, generateSeed bool) (err error) {
 	log.Info("Loading map file from: ", mapFile)
 	columnMap, err := gonymizer.LoadConfigSkeleton(mapFile)
 	if err != nil {
@@ -133,7 +133,7 @@ func process(dumpFile, mapFile, processedDumpFile string, generateSeed bool, s3f
 	}
 
 	log.Info("Processing dump file: ", dumpFile)
-	err = gonymizer.ProcessDumpFile(columnMap, dumpFile, processedDumpFile, postProcessFile, generateSeed)
+	err = gonymizer.ProcessDumpFile(columnMap, dumpFile, processedFile, postProcessFile, generateSeed)
 	if err != nil {
 		return err
 	}
@@ -141,14 +141,14 @@ func process(dumpFile, mapFile, processedDumpFile string, generateSeed bool, s3f
 	// Upload the processed file to S3 (iff the user selected this option and it is valid)
 	log.Info("S3 scheme: ", s3file.Scheme)
 	if s3file.Scheme == "s3" {
-		log.Infof("🚛 Uploading '%s' => S3: %s\n", processedDumpFile, s3file.URL)
+		log.Infof("🚛 Uploading '%s' => S3: %s\n", processedFile, s3file.URL)
 		sess, err := session.NewSession(&aws.Config{Region: aws.String(s3file.Region)})
 		if err != nil {
 			return err
 		}
 
-		if err = gonymizer.AddFileToS3(sess, processedDumpFile, s3file); err != nil {
-			log.Errorf("Unable to upload '%s' => '%s'", processedDumpFile, s3file.URL)
+		if err = gonymizer.AddFileToS3(sess, processedFile, s3file); err != nil {
+			log.Errorf("Unable to upload '%s' => '%s'", processedFile, s3file.URL)
 			return err
 		}
 	}
